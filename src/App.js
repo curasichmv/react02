c2d5d1f34a34552d6afb58edb5a6e5f1089a5475
curasichmv <chavezmartha808@gmail.com>
2023-11-14 14:01:26 -0500

    v1. modifique app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import C06componente from './component/C06componente';
+/*import C06componente from './component/C06componente';
 import C05contador from './component/C05contador';
 import C04doblecontador from './component/C04doblecontador';
 import C03variable from './component/C03variable';
 import C02operador from './component/C02operador';
-import C01matriz from './component/C01matriz';
+import C01matriz from './component/C01matriz';*/
 
 function App() {
   return (
     <div>
       •Saludo con sus nombres y apellidos
-      <h3>HOLA ATODOS ES UN SALUDO DE "ENRIQUE MAMANI VARGAS"</h3>
-      • Mostrar la fecha actual y el turno
-      <h3>FECHA: 08/11/2023</h3>
+      <h3>ISEP HONORIO DELGADO ESPINOZA</h3>
+      <h3>Martha Curasi Chavez</h3>
       <h3>TURNO: NOCHE SEXTO SEMESTRE</h3>
       
 
