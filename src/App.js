0a241dbc79760cae22ea5273dd3619c33a12e030
curasichmv <chavezmartha808@gmail.com>
2023-11-14 13:02:47 -0500

    vs. jdahjh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import AppForm from './componente/AppForm';
 import logo from './logo.svg';
-//import './App.css';
-import Componente from './pagina/Componente';
+import './App.css';
+import C06componente from './component/C06componente';
+import C05contador from './component/C05contador';
+import C04doblecontador from './component/C04doblecontador';
+import C03variable from './component/C03variable';
+import C02operador from './component/C02operador';
+import C01matriz from './component/C01matriz';
 
 function App() {
   return (
-    <div style={{width:"350px", background:"greenyellow",padding:"10px"}}>
-     <AppForm />
-     <p>No. 1 Juan Manuel x A</p>
-     <p>No. 1 Rosa Maria x A</p>
+    <div>
+      •Saludo con sus nombres y apellidos
+      <h3>HOLA ATODOS ES UN SALUDO DE "ENRIQUE MAMANI VARGAS"</h3>
+      • Mostrar la fecha actual y el turno
+      <h3>FECHA: 08/11/2023</h3>
+      <h3>TURNO: NOCHE SEXTO SEMESTRE</h3>
+      
+
+
     </div>
   );
 }
